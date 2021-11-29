201bba57b66aaba391c78e758e6fe61acb24614d
Tyler Norman <tnorman600@gmail.com>
2021-11-29 14:26:39 -0700

    finished adding add, update, and delete related fucntions. Added util folder and populateArray.js to aid inquirer in displaying options a user needs when updating/deleting info.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 const caseFunction = require('./db/switchCaseFunctions');
+const {deptArrFill, roleArrFill, employeeArrFill, managerArrFill} = require('./utils/populateArray');
+
+let deptArr = deptArrFill();
+let roleArr = roleArrFill();
+let managerArr = managerArrFill();
+let employeeArr = employeeArrFill();
 
 const promptUser = async () => {
     inquirer.prompt([
@@ -18,8 +24,10 @@ const promptUser = async () => {
                 "Add a new department",
                 "Add a new role",
                 "Add a new employee",
-                "Update an employee's info",
-                "Delete a department, role, or employee",
+                "Update an employee's role",
+                "Delete a department",
+                "Delete a role",
+                "Delete an employee",
                 "Quit"
             ]
         }
@@ -27,32 +35,32 @@ const promptUser = async () => {
         switch (userInput.viewOrUpdate) {
             case "View all departments":
                 caseFunction.viewAllDepartments();
-                setTimeout(()=>{promptUser(),1000});
+                setTimeout(()=>{promptUser(),666});
                 break;
 
             case "View all roles":
                 caseFunction.viewAllRoles();
-                setTimeout(()=>{promptUser(),1000});
+                setTimeout(()=>{promptUser(),666});
                 break;
 
             case "View all employees":
                 caseFunction.viewAllEmployees();
-                setTimeout(()=>{promptUser(),1000});
+                setTimeout(()=>{promptUser(),666});
                 break;
 
             case "View employees by department":
                 caseFunction.viewEmployeesByDepartment();
-                setTimeout(()=>{promptUser(),1000});
+                setTimeout(()=>{promptUser(),666});
                 break;
 
             case "View employees by manager":
                 caseFunction.viewEmployeesByManager();
-                setTimeout(()=>{promptUser(),1000});
+                setTimeout(()=>{promptUser(),666});
                 break;
 
             case "View department budget info":
                 caseFunction.viewDepartmentBudgetInfo();
-                setTimeout(()=>{promptUser(),1000});
+                setTimeout(()=>{promptUser(),666});
                 break;
 
             case "Add a new department":
@@ -67,14 +75,22 @@ const promptUser = async () => {
                 addNewEmployee();
                 break;
 
-            case "Update an employee's info":
-                updateEmployeeInfo();
+            case "Update an employee's role":
+                updateEmployeeRole();
                 break;
 
-            case "Delete a department, role, or employee":
-                deleteDepartmentRoleEmployee();
+            case "Delete a department":
+                deleteDepartment();
                 break;
 
+            case "Delete a role":
+                deleteRole();
+                break;
+
+            case "Delete an employee":
+                deleteEmployee();
+                break;
+            
             case "Quit":
                 db.end();
                 console.log(`
@@ -101,14 +117,6 @@ const init = () => {
     });
 };
 
-const viewAllDepartments = () => {
-    db.query(`SELECT * FROM departments;`, 
-    function(err, res) {
-        if (err) throw err
-        console.table(res);
-    });
-};
-
 const addNewDepartment = () => {
     inquirer.prompt([
         {
@@ -125,52 +133,164 @@ const addNewDepartment = () => {
           }
         }
     ]).then(answer => {
-            const sql = `INSERT INTO departments (dept_name) VALUES (?)`;
-            db.query(sql, answer.addNewDept, (err, result) => {
-                if (err) throw err;
-                console.log('New department added: ' + answer.addNewDept); 
-            });
-        }).then(() => promptUser());
+        const sql = `INSERT INTO departments (dept_name) VALUES (?)`;
+        db.query(sql, answer.addNewDept, (err, result) => {
+            if (err) throw err;
+            console.log(`New department added: ${answer.addNewDept}`); 
+        });
+    }).then(() => promptUser());
 };
 
 const addNewRole = () => {
-    viewAllDepartments();
     inquirer.prompt([
         {
-            
+            type: 'input',
+            name: 'title',
+            message: 'Name new role: ',
+            validate: title => {
+                if (title) {
+                    return true;
+                } else {
+                    console.log('Please enter a role title');
+                    return false;
+                };
+            }
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'Enter new role salary: ',
+            validate: salary => {
+                if (isNaN(salary) || !salary) {
+                    console.log('Please enter a salary');
+                    return false;
+                } else {
+                    return true;
+                };
+            }
+        },
+        {
+            type: 'list',
+            name: 'department',
+            message: 'Enter department ID number for new role: ',
+            choices: deptArr
         }
     ]).then(answer => {
-        
+        const sql = `INSERT INTO roles (title, salary, dept_id) VALUES (?, ?, ?)`; 
+        const params = [answer.title, answer.salary, answer.department];
+        db.query(sql, params, (err, result) => {
+            if (err) throw err;
+            console.log(`New role added: ${answer.title}`); 
+        });
     }).then(() => promptUser());
 };
 
 const addNewEmployee = () => {
     inquirer.prompt([
         {
-
+        type: 'input',
+        name: 'firstName',
+        message: "Enter employees first name: "
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: "Enter employees last name: "
+        },
+        {
+            type: 'list',
+            name: 'role',
+            message: "Select employees role: ",
+            choices: roleArr
+        },
+        {
+            type: 'list',
+            name: 'manager',
+            message: "Select employees manager: ",
+            choices: managerArr
         }
     ]).then(answer => {
-
+        const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`; 
+        const params = [answer.firstName, answer.lastName, answer.role, answer.manager];
+        db.query(sql, params, (err, result) => {
+            if (err) throw err;
+            console.log(`New employee added: ${answer.firstName} ${answer.lastName}`); 
+        });
     }).then(() => promptUser());
 };
 
-const updateEmployeeInfo = () => {
+const updateEmployeeRole = () => {
     inquirer.prompt([
         {
-
+            type: 'list',
+            name: 'employee',
+            message: 'Which employee would you like to update?',
+            choices: employeeArr
+        },
+        {
+            type: 'list',
+            name: 'newRole',
+            message: 'What is the employees new role?',
+            choices: roleArr
         }
     ]).then(answer => {
-
+        const sql = `UPDATE employees SET role_id = ? WHERE id = ?`
+        const params = [answer.newRole, answer.employee]
+        db.query(sql, params, (err, res) => {
+            if (err) throw err;
+            console.log(`Role change complete`); 
+        });
     }).then(() => promptUser());
 };
 
-const deleteDepartmentRoleEmployee = () => {
+const deleteDepartment = () => {
     inquirer.prompt([
         {
-
+            type: 'list',
+            name: 'department',
+            message: 'Select department to delete: ',
+            choices: deptArr
         }
     ]).then(answer => {
+        const sql = `DELETE FROM departments WHERE id = ${answer.department}`;
+        db.query(sql, (err, row) => {
+            if (err) throw err;
+            console.table(deptArr);
+        });
+    }).then(() => promptUser());
+};
 
+const deleteRole = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Select role to delete: ',
+            choices: roleArr
+        }
+    ]).then(answer => {
+        const sql = `DELETE FROM roles WHERE id = ${answer.role}`;
+        db.query(sql, (err, row) => {
+            if (err) throw err;
+            console.log(`Role ${answer.role} deleted`);
+        });
+    }).then(() => promptUser());
+};
+
+const deleteEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'employee',
+            message: 'Select employee to delete: ',
+            choices: employeeArr
+        }
+    ]).then(answer => {
+        const sql = `DELETE FROM employees WHERE id = ${answer.employee}`;
+        db.query(sql, (err, row) => {
+            if (err) throw err;
+            console.log('Employee deleted');
+        });
     }).then(() => promptUser());
 };
 
